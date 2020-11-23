650037f4d46c8b2246f44a43c7ed8d21c76e5720
Ratul Rahman Rudra <ratul.rahman.rrr@gmail.com>
2020-11-23 20:49:09 +0600

    pre_install module

---
+++ pre_install.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+
+sp.call('pip install pyttsx3')
+sp.call('pip install discord')
+sp.call('pip install youtube-search')
+sp.call('pip install SpeechRecognition')
+sp.call('pip install wikipedia')
+sp.call('pip install urllib3')
+
