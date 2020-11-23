4f556e472fd92298f014914a5367a199f63c0a12
Ratul Rahman Rudra <ratul.rahman.rrr@gmail.com>
2020-11-23 21:49:01 +0600

    py_audio

--- pre_install.py
+++ pre_install.py
@@ -7,3 +7,5 @@ sp.call('pip install SpeechRecognition')
 sp.call('pip install wikipedia')
 sp.call('pip install urllib3')
 
+pyAudioFile = open('./module/PyAudio-0.2.11-cp39-cp39-win_amd64.whl', 'r')
+sp.call('pip install '+pyAudioFile.name)
